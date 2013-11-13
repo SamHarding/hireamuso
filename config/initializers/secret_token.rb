068f4d32707fd590a9aec2083cdf422a04ad4121
Sam Harding <samdharding@gmail.com>
2013-11-13 12:59:25 +1100

    Creating new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hireamuso::Application.config.secret_key_base = 'ca13db17cf28017ddde817320ced3248f5754f6085a7da52b23aac5bb9fa01627e295d55895a737af9c09e775cf90475acac13aa3c6cdbe85820776e387eb5b5'
